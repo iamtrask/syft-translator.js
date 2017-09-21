a7e6ff45dcdb0296e3568939f1b779dff7a5a0b0
Robert Plummer <robertleeplummerjr@gmail.com>
2017-09-21 11:57:05 -0400

    wire up dependencies and echo js to command line

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const sync = require('./sync');
 const parse = require('filbert/filbert_loose').parse_dammit;
 const glob = require('glob');
+const astring = require('astring');
 
 sync();
 
 glob('temp/**/*.py', (err, files) => {
   for (let i = 0; i < files.length; i++) {
-    console.log(JSON.stringify(parse(fs.readFileSync(files[i]).toString())));
+    console.log(astring.generate(parse(fs.readFileSync(files[i]).toString())));
   }
 });
